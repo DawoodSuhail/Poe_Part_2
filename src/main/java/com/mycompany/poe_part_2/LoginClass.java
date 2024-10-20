3593b69f44de2cd171fd2ca9e5b87ba6fdf70a53
Dawood <Dawood@DaHoodLaaitie>
2024-10-20 15:54:05 +0200

    LoginClass_new

--- src/main/java/com/mycompany/poe_part_2/LoginClass.java
+++ src/main/java/com/mycompany/poe_part_2/LoginClass.java
@@ -2,9 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.poepart1;
+package com.mycompany.poe_part_2;
 import java.util.regex.Pattern;
-
 /**
  *
  * @author Dawood
@@ -54,3 +53,4 @@ public class LoginClass {
         return loginSuccess ? "Welcome! It is great to see you again." : "Username or password incorrect, please try again.";
     }
 }
+
