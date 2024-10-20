cdc8ed7305affe4bc26d025ec587b3d7bc075ccd
Dawood <Dawood@DaHoodLaaitie>
2024-10-20 15:44:02 +0200

    Add VAriables

---
+++ src/main/java/com/mycompany/poe_part_2/Poe_Part_2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poe_part_2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dawood
+ */
+public class Poe_Part_2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LoginClass login = new LoginClass(); // Create an instance of LoginClass
+
+        // Variables to store user input
+        String firstName;
+        String lastName;
+        String username;
+        String password;
+    }
+}
